a87c23872973e148025a44d37823f3a6f8ffd57c
hoangnhat42 <hoangnhat.contact@gmail.com>
2022-03-30 18:00:19 +0700

    setup conversation in the main screen

--- app/src/main/java/com/example/appchat3n/MainActivity.java
+++ app/src/main/java/com/example/appchat3n/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.appchat3n;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,12 +10,51 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.example.appchat3n.databinding.ActivityMainBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    FirebaseDatabase database;
+    ArrayList<User> users;
+    UsersAdapter usersAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        database = FirebaseDatabase.getInstance();
+        users = new ArrayList<>();
+
+        usersAdapter = new UsersAdapter(this, users);
+      //  binding.recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        binding.recyclerView.setAdapter(usersAdapter);
+        database.getReference().child("users").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                users.clear();
+                for(DataSnapshot snapshot1 : snapshot.getChildren()) {
+                    User user = snapshot1.getValue(User.class);
+                        users.add(user);
+                }
+                //binding.recyclerView.hideShimmerAdapter();
+                usersAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     @Override
